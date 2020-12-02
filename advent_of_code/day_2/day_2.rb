47a80884d13ff7ac9bf77a011d68bded026535ca
Phillip Strom <phillip.strom@gmail.com>
2020-12-02 12:40:06 -0700

    Refactor input so lines are split in methods

--- advent_of_code/day_2/day_2.rb
+++ advent_of_code/day_2/day_2.rb
@@ -2,27 +2,22 @@
 
 lines = File.readlines('input.txt')
 
-inputs = lines.map do |line|
-  key, value = line.chomp.split(':')
-end
-
-good_passwords = inputs.select do |policy, password|
-  times, letter = policy.split(' ')
-  range = Range.new(*times.split('-').map(&:to_i))
+good_passwords = lines.count do |line|
+	policy, password = line.split(': ')
+  positions, letter = policy.split(' ')
+  range = Range.new(*positions.split('-').map(&:to_i))
   letter_count = password.count(letter)
   range.include?(letter_count)
 end
 
-puts good_passwords.size
+puts good_passwords
 
 # Part 2
-
-good_passwords = inputs.select do |policy, password|
-  times, letter = policy.split(' ')
-  position1, position2 = times.split('-').map(&:to_i)
-  require "pry"; binding.pry
-  (password[position1 - 1] == letter) ^ (password[position2 - 1] == letter)
+good_passwords = lines.count do |line|
+	policy, password = line.split(': ')
+	positions, letter = policy.split(' ')
+	position1, position2 = positions.split('-').map(&:to_i)
+	(letter == password[position1 - 1]) ^ (letter == password[position2 - 1])
 end
-require "pry"; binding.pry
 
-puts good_passwords.size
+puts good_passwords
