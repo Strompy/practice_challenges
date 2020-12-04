d87a1fa47bb6a0ba73fddd72d03f364861aadf15
Phillip Strom <phillip.strom@gmail.com>
2020-12-04 11:42:33 -0700

    Add check for byr

--- advent_of_code/day_4/day_4.rb
+++ advent_of_code/day_4/day_4.rb
@@ -14,6 +14,37 @@ def check_valid(passport)
   end
 end
 
+def check_extra_valid(passport)
+  return true if passport.keys.length == 8
+  if passport.keys.length == 7 && passport.keys.include?("cid")
+    return false
+  elsif passport.keys.length == 7
+    return true
+  else
+    return false
+  end
+end
+
+def check_byr(credential)
+  credential.length == 4 && (1920..2002).include?(credential.to_i)
+end
+
+def check_hgt(credential)
+
+end
+
+def check_hcl(credential)
+
+end
+
+def check_ecl(credential)
+
+end
+
+def check_pid(credential)
+
+end
+
 def number_of_valid(passports)
   passports.count do |passport|
     check_valid(passport)
