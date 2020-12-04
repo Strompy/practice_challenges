1dc45778f39f5879494c777df351da7d54b18015
Phillip Strom <phillip.strom@gmail.com>
2020-12-04 12:33:48 -0700

    Add all the methods

--- advent_of_code/day_4/day_4.rb
+++ advent_of_code/day_4/day_4.rb
@@ -16,14 +16,20 @@ def check_valid(passport)
 end
 
 def check_extra_valid(passport)
-  return true if passport.keys.length == 8
-  if passport.keys.length == 7 && passport.keys.include?("cid")
-    return false
-  elsif passport.keys.length == 7
-    return true
+  if check_valid(passport) && valid_credentials?(passport)
+    true
   else
-    return false
+    false
   end
+end
+
+def valid_credentials?(passport)
+  check_byr(passport['byr']) &&
+  check_iyr(passport['iyr']) &&
+  check_eyr(passport['eyr']) &&
+  check_hgt(passport['hgt']) &&
+  check_hcl(passport['hcl']) &&
+  check_pid(passport['pid'])
 end
 
 def check_byr(credential)
@@ -63,7 +69,7 @@ def check_ecl(credential)
 end
 
 def check_pid(credential)
-
+  credential.match?(/\A\d{9}\z/)
 end
 
 def number_of_valid(passports)
@@ -73,3 +79,32 @@ def number_of_valid(passports)
 end
 
 p number_of_valid(passports)
+
+def number_of_extra_valid(passports)
+  passports.count do |passport|
+    check_extra_valid(passport)
+  end
+end
+
+p number_of_extra_valid(passports)
+
+
+
+# passports = File.read("input.txt").split("\n\n")
+#
+# passports.map! do |passport|
+#   passport.scan(/(\w+{3})\:(\S+)/).to_h
+# end
+#
+# puts passports.select { |passport|
+#   (passport.keys & %w{byr iyr eyr hgt hcl ecl pid}).length == 7 &&
+#   passport['byr'].to_i.between?(1920, 2002) &&
+#   passport['iyr'].to_i.between?(2010, 2020) &&
+#   passport['eyr'].to_i.between?(2020, 2030) &&
+#   passport['hcl'] =~ /^\#[0-9a-f]{6}$/ &&
+#   %w{amb blu brn gry grn hzl oth}.include?(passport['ecl']) &&
+#   passport['pid'] =~ /^\d{9}$/ &&
+#   (
+#     (passport['hgt'].end_with?('cm') && passport['hgt'].to_i.between?(150, 193)) || (passport['hgt'].end_with?('in') && passport['hgt'].to_i.between?(59, 76))
+#   )
+# }.size
