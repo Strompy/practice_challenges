09a70fc92c459e056414a83f1a88a9df2b4d3326
Phillip Strom <phillip.strom@gmail.com>
2020-12-06 15:33:39 -0700

    Update variable names

--- advent_of_code/day_6/day_6.rb
+++ advent_of_code/day_6/day_6.rb
@@ -1,10 +1,24 @@
-counter = 0
+part1 = 0
 File.read("input.txt").split("\n\n").each do |line|
   tracker = Hash.new(0)
   line.delete("\n").chars.each do |char|
     tracker[char] += 1
   end
-  counter += tracker.size
+  part1 += tracker.size
 end
 
-puts "Total Answers: #{counter}"
+puts "Total Answers: #{part1}"
+
+part2 = 0
+File.read("input.txt").split("\n\n").each do |line|
+  tracker = Hash.new(0)
+  party_size = line.count("\n") + 1
+  line.delete("\n").chars.each do |char|
+    tracker[char] += 1
+  end
+  tracker.each do |char, count|
+    part2 += 1 if count == party_size
+  end
+end
+
+puts "All Yes Answers: #{part2}"
