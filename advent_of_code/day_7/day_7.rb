984f8a97bc071ee217a0e62af44a448b416cbcc6
Phillip Strom <phillip.strom@gmail.com>
2020-12-07 13:35:27 -0700

    Solve part 1

--- advent_of_code/day_7/day_7.rb
+++ advent_of_code/day_7/day_7.rb
@@ -1,15 +1,37 @@
 contained_by = Hash.new
+holds = Hash.new
 
-File.readlines('test.txt').each do |line|
+File.readlines('input.txt').each do |line|
   bag, rules = line.chomp.split(' bags contain')
-  if rules.include?("no other bags")
-    contained_by[bag] ||= []
-    next
-  end
-  rules.delete_suffix('.').split(',').each do |rule|
-    contained_by[bag] ||= []
+
+  contained_by[bag] = rules.delete_suffix('.').split(',').map do |rule|
     details = rule.match(/(?<count>\d+) (?<color>.+) bags?/)
-    contained_by[bag] << details[:color]
+    if rule.include?("no other bags")
+       {}
+    else
+       {
+        "count" => details[:count],
+        "color" => details[:color]
+      }
+    end
+    # contained_by[bag] ||= []
+    # contained_by[bag] << details[:color]
+    # holds[details[:color]] ||= []
+    # holds[details[:color]] << bag
   end
 end
-require "pry"; binding.pry
+# require "pry"; binding.pry
+def bags_holding(contained_by, color, tracker)
+  contained_by.each do |bag, rules|
+    rules.each do |rule|
+      if rule['color'] == color
+        tracker << bag
+        bags_holding(contained_by, bag, tracker)
+      end
+    end
+  end
+  tracker
+end
+
+bags = []
+puts bags_holding(contained_by, 'shiny gold', bags).uniq.size
