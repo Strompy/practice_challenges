4905b072e6ab8e4c2d7de7c7c873e0f2c04309ba
Phillip Strom <phillip.strom@gmail.com>
2020-12-09 10:36:38 -0700

    Part 2 in progress

--- advent_of_code/day_8/day_8.rb
+++ advent_of_code/day_8/day_8.rb
@@ -1,5 +1,5 @@
 class GameBuddy
-  attr_accessor :instructions, :accumulator, :current_instruction
+  attr_accessor :instructions, :accumulator, :current_instruction, :executed
 
   def initialize(instructions)
     @instructions = instructions
@@ -8,9 +8,10 @@ class GameBuddy
     @executed = {}
   end
 
-  def read_instructions
+  def read_instructions(instructions)
     until current_instruction >= instructions.size
       index = current_instruction
+      require "pry"; binding.pry
       break if !@executed[index].nil?
       operator(instructions[index])
       @executed[index] = true
@@ -49,10 +50,103 @@ class GameBuddy
   def nope(argument)
     @current_instruction += 1
   end
+
+  def change_instructions
+    instructions.to_enum.with_index.reverse_each do |instruction, index|
+      @accumulator = 0
+      @current_instruction = 0
+      changed = instructions.clone
+      next if instruction.include?('acc')
+      # require "pry"; binding.pry
+      if instruction.include?('nop')
+        changed[index] = instruction.gsub('nop', 'jmp')
+      else
+        changed[index] = instruction.include?('nop') ? instruction.gsub!('nop', 'jmp') : instruction.gsub!('jmp', 'nop')
+      end
+      read_instructions(changed)
+      require "pry"; binding.pry
+      return accumulator if current_instruction >= instruction.size
+    end
+    # code.each.with_index do |_, loc|
+    #   next unless %i{jmp nop}.include?(code[loc][0])
+    #
+    #   code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
+    #   res = execute(code)
+    #   code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
+    #
+    #   puts "#{loc} - #{res}" if res
+    # end
+  end
+
+  # def change_instructions
+  #   last_neg_jmp = 0
+  #   last_neg_jmp_index = 0
+  #   instructions.each_with_index do |instruction, i|
+  #     if instruction.include?('jmp -')
+  #       last_neg_jmp_index, last_neg_jmp = i, instruction
+  #     end
+  #   end
+  #   if executed[last_neg_jmp_index] == true
+  #     print "changing last neg jmp to nop"
+  #     @instructions[last_neg_jmp_index] = instructions[last_neg_jmp_index].gsub('jmp', 'nop')
+  #     return
+  #   end
+  #
+  #   instructions.to_enum.with_index.reverse_each do |instruction, i|
+  #     if executed[i] && instruction.include?('nop') && (i + instruction[/\d+/].to_i) > last_neg_jmp_index
+  #       instruction.gsub!('nop', 'jmp')
+  #       print "change nop to jmp"
+  #       return
+  #     elsif !executed[i] && instruction.include?('jmp') && (i + instruction[/\d+/].to_i) > last_neg_jmp_index
+  #       # a jmp that wasn't hit that would land you there, preceded by a jmp that was hit
+  #       # (so by nopping the preceding jmp you hit the one that takes you to the end)
+  #     end
+  #   end
+  # end
 end
 
 input = File.read("input.txt").split("\n")
 
 gamebuddy = GameBuddy.new(input)
-puts 'Part 1:'
-puts gamebuddy.read_instructions
+# puts 'Part 1:'
+# puts gamebuddy.read_instructions
+# puts 'Part 2:'
+# gamebuddy.change_instructions
+# puts gamebuddy.read_instructions
+
+
+
+# Parser and lexer ;-)
+code = File.readlines('input.txt')
+         .map(&:split)
+         .map { |is| [is[0].to_sym, is[1].to_i] }
+
+# Interpreter
+def execute(code, no_loops = false)
+  pc = acc = cycles = 0
+  coverage = []
+
+  while (cycles += 1) < 1000
+    return acc if pc >= code.length || (coverage[pc] && no_loops)
+    coverage[pc] = true
+
+    ins, op = code[pc]
+    acc += op    if ins == :acc
+    pc += op - 1 if ins == :jmp
+    pc += 1
+  end
+end
+
+# Run code for part one
+puts execute(code, true)
+
+# Change code for part two
+code.each.with_index do |_, loc|
+  next unless %i{jmp nop}.include?(code[loc][0])
+
+  code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
+  res = execute(code)
+  code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
+
+  puts "#{loc} - #{res}" if res
+end
--- advent_of_code/day_8/day_8_test.rb
+++ advent_of_code/day_8/day_8_test.rb
@@ -16,79 +16,97 @@ class GameBuddyTest < Minitest::Test
 
     @gameboy = GameBuddy.new(@test)
   end
-  def test_it_exists_with_attributes
-    test = ['nop +0',
+  # def test_it_exists_with_attributes
+  #   test = ['nop +0',
+  #   'acc +1',
+  #   'jmp +4',
+  #   'acc +3',
+  #   'jmp -3',
+  #   'acc -99',
+  #   'acc +1',
+  #   'jmp -4',
+  #   'acc +6']
+  #
+  #   console = GameBuddy.new(test)
+  #   assert_instance_of GameBuddy, console
+  #   assert_equal test, console.instructions
+  #   assert_equal 0, console.accumulator
+  #   assert_equal 0, console.current_instruction
+  # end
+  #
+  # def test_it_can_accumulate
+  #   assert_equal 0, @gameboy.accumulator
+  #
+  #   @gameboy.accumulate('+2')
+  #   assert_equal 2, @gameboy.accumulator
+  # end
+  #
+  # def test_it_can_jump
+  #   assert_equal 0, @gameboy.current_instruction
+  #
+  #   @gameboy.jump('+2')
+  #   assert_equal 2, @gameboy.current_instruction
+  #
+  #   @gameboy.jump('-1')
+  #   assert_equal 1, @gameboy.current_instruction
+  # end
+  #
+  # def test_it_can_nope
+  #   assert_equal 0, @gameboy.current_instruction
+  #
+  #   @gameboy.nope('-4')
+  #   assert_equal 1, @gameboy.current_instruction
+  #
+  #   @gameboy.nope('+32')
+  #   assert_equal 2, @gameboy.current_instruction
+  # end
+  #
+  # def test_it_can_operator
+  #   assert_equal 0, @gameboy.accumulator
+  #   assert_equal 0, @gameboy.current_instruction
+  #
+  #   @gameboy.operator('acc +19')
+  #   assert_equal 19, @gameboy.accumulator
+  #   assert_equal 1, @gameboy.current_instruction
+  #
+  #   @gameboy.operator('jmp +26')
+  #   assert_equal 19, @gameboy.accumulator
+  #   assert_equal 27, @gameboy.current_instruction
+  #
+  #   @gameboy.operator('nop -17')
+  #   assert_equal 19, @gameboy.accumulator
+  #   assert_equal 28, @gameboy.current_instruction
+  # end
+  #
+  # def test_it_can_read_instructions
+  #   assert_equal 0, @gameboy.accumulator
+  #   assert_equal 0, @gameboy.current_instruction
+  #
+  #   @gameboy.read_instructions(@test)
+  #
+  #   assert_equal 5, @gameboy.accumulator
+  # end
+
+  def test_it_can_change_instructions
+    assert_equal 0, @gameboy.accumulator
+    assert_equal 0, @gameboy.current_instruction
+
+    @gameboy.read_instructions(@test)
+
+    assert_equal 5, @gameboy.accumulator
+    expected = {0=>true, 1=>true, 2=>true, 6=>true, 7=>true, 3=>true, 4=>true}
+    assert_equal expected, @gameboy.executed
+
+    corrected = ['nop +0',
     'acc +1',
     'jmp +4',
     'acc +3',
     'jmp -3',
     'acc -99',
     'acc +1',
-    'jmp -4',
+    'nop -4',
     'acc +6']
-
-    console = GameBuddy.new(test)
-    assert_instance_of GameBuddy, console
-    assert_equal test, console.instructions
-    assert_equal 0, console.accumulator
-    assert_equal 0, console.current_instruction
-  end
-
-  def test_it_can_accumulate
-    assert_equal 0, @gameboy.accumulator
-
-    @gameboy.accumulate('+2')
-    assert_equal 2, @gameboy.accumulator
-  end
-
-  def test_it_can_jump
-    assert_equal 0, @gameboy.current_instruction
-
-    @gameboy.jump('+2')
-    assert_equal 2, @gameboy.current_instruction
-
-    @gameboy.jump('-1')
-    assert_equal 1, @gameboy.current_instruction
-  end
-
-  def test_it_can_nope
-    assert_equal 0, @gameboy.current_instruction
-
-    @gameboy.nope('-4')
-    assert_equal 1, @gameboy.current_instruction
-
-    @gameboy.nope('+32')
-    assert_equal 2, @gameboy.current_instruction
-  end
-
-  def test_it_can_operator
-    assert_equal 0, @gameboy.accumulator
-    assert_equal 0, @gameboy.current_instruction
-
-    @gameboy.operator('acc +19')
-    assert_equal 19, @gameboy.accumulator
-    assert_equal 1, @gameboy.current_instruction
-
-    @gameboy.operator('jmp +26')
-    assert_equal 19, @gameboy.accumulator
-    assert_equal 27, @gameboy.current_instruction
-
-    @gameboy.operator('nop -17')
-    assert_equal 19, @gameboy.accumulator
-    assert_equal 28, @gameboy.current_instruction
-  end
-
-  def test_it_can_read_instructions
-    assert_equal 0, @gameboy.accumulator
-    assert_equal 0, @gameboy.current_instruction
-
-    @gameboy.read_instructions
-
-    assert_equal 5, @gameboy.accumulator
-  end
-
-  def test_it_can_escape_loop
-
-
+    @gameboy.change_instructions
+    assert_equal corrected, @gameboy.instructions
   end
 end
