b2e3283e4e402cac171038bd4b7b2608c0f68ed6
Phillip Strom <phillip.strom@gmail.com>
2020-12-09 11:45:56 -0700

    Solve part 1

--- advent_of_code/day_9/day_9.rb
+++ advent_of_code/day_9/day_9.rb
@@ -1,9 +1,11 @@
 test = (1..26).to_a
 test << 50
 
+data = File.read("input.txt").split("\n").map(&:to_i)
+
 invalid = 0
-test.drop(26).each_with_index do |num, index|
-  valid = test[index..index+24].permutation(2).to_a.find { |combo| combo.sum == num }
+data.drop(25).each_with_index do |num, index|
+  valid = data[index..index+24].combination(2).to_a.find { |combo| combo.sum == num }
   invalid = num if valid.nil?
 end
 
