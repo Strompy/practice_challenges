4792b18f424d470ff7135ee7560c3e989215290b
Phillip Strom <phillip.strom@gmail.com>
2020-12-09 12:54:01 -0700

    Solve part 2

--- advent_of_code/day_9/day_9.rb
+++ advent_of_code/day_9/day_9.rb
@@ -1,6 +1,3 @@
-test = (1..26).to_a
-test << 50
-
 data = File.read("input.txt").split("\n").map(&:to_i)
 
 invalid = 0
@@ -10,3 +7,14 @@ data.drop(25).each_with_index do |num, index|
 end
 
 puts "Part 1: #{invalid}"
+
+part_2 = 0
+(2..data.size).each do |size|
+  data.each_cons(size) do |array|
+    if array.reduce(:+) == invalid
+      part_2 = array.min + array.max
+    end
+  end
+end
+
+puts "Part 2: #{part_2}"
