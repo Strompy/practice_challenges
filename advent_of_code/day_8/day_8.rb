7d5566b3ca5abb2afbbf91005104ccc5d345789c
Phillip Strom <phillip.strom@gmail.com>
2020-12-09 14:02:36 -0700

    Get change instruction working

--- advent_of_code/day_8/day_8.rb
+++ advent_of_code/day_8/day_8.rb
@@ -11,7 +11,6 @@ class GameBuddy
   def read_instructions(instructions)
     until current_instruction >= instructions.size
       index = current_instruction
-      require "pry"; binding.pry
       break if !@executed[index].nil?
       operator(instructions[index])
       @executed[index] = true
@@ -38,7 +37,10 @@ class GameBuddy
   end
 
   def jump(argument)
-    if argument[0] == '+'
+    if argument[/\d+/].to_i == 0
+      require "pry"; binding.pry
+      @current_instruction += 1
+    elsif argument[0] == '+'
       @current_instruction += argument[/\d+/].to_i
     elsif argument[0] == '-'
       @current_instruction -= argument[/\d+/].to_i
@@ -55,98 +57,62 @@ class GameBuddy
     instructions.to_enum.with_index.reverse_each do |instruction, index|
       @accumulator = 0
       @current_instruction = 0
+      @executed = {}
       changed = instructions.clone
       next if instruction.include?('acc')
-      # require "pry"; binding.pry
       if instruction.include?('nop')
         changed[index] = instruction.gsub('nop', 'jmp')
       else
-        changed[index] = instruction.include?('nop') ? instruction.gsub!('nop', 'jmp') : instruction.gsub!('jmp', 'nop')
+        changed[index] = instruction.gsub('jmp', 'nop')
       end
       read_instructions(changed)
-      require "pry"; binding.pry
-      return accumulator if current_instruction >= instruction.size
+      break if current_instruction >= instructions.size
     end
-    # code.each.with_index do |_, loc|
-    #   next unless %i{jmp nop}.include?(code[loc][0])
-    #
-    #   code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
-    #   res = execute(code)
-    #   code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
-    #
-    #   puts "#{loc} - #{res}" if res
-    # end
   end
-
-  # def change_instructions
-  #   last_neg_jmp = 0
-  #   last_neg_jmp_index = 0
-  #   instructions.each_with_index do |instruction, i|
-  #     if instruction.include?('jmp -')
-  #       last_neg_jmp_index, last_neg_jmp = i, instruction
-  #     end
-  #   end
-  #   if executed[last_neg_jmp_index] == true
-  #     print "changing last neg jmp to nop"
-  #     @instructions[last_neg_jmp_index] = instructions[last_neg_jmp_index].gsub('jmp', 'nop')
-  #     return
-  #   end
-  #
-  #   instructions.to_enum.with_index.reverse_each do |instruction, i|
-  #     if executed[i] && instruction.include?('nop') && (i + instruction[/\d+/].to_i) > last_neg_jmp_index
-  #       instruction.gsub!('nop', 'jmp')
-  #       print "change nop to jmp"
-  #       return
-  #     elsif !executed[i] && instruction.include?('jmp') && (i + instruction[/\d+/].to_i) > last_neg_jmp_index
-  #       # a jmp that wasn't hit that would land you there, preceded by a jmp that was hit
-  #       # (so by nopping the preceding jmp you hit the one that takes you to the end)
-  #     end
-  #   end
-  # end
 end
 
 input = File.read("input.txt").split("\n")
 
 gamebuddy = GameBuddy.new(input)
-# puts 'Part 1:'
-# puts gamebuddy.read_instructions
-# puts 'Part 2:'
-# gamebuddy.change_instructions
-# puts gamebuddy.read_instructions
+puts 'Part 1:'
+puts gamebuddy.read_instructions(input)
+puts 'Part 2:'
+gamebuddy.change_instructions
+puts gamebuddy.accumulator
 
 
 
-# Parser and lexer ;-)
-code = File.readlines('input.txt')
-         .map(&:split)
-         .map { |is| [is[0].to_sym, is[1].to_i] }
-
-# Interpreter
-def execute(code, no_loops = false)
-  pc = acc = cycles = 0
-  coverage = []
-
-  while (cycles += 1) < 1000
-    return acc if pc >= code.length || (coverage[pc] && no_loops)
-    coverage[pc] = true
-
-    ins, op = code[pc]
-    acc += op    if ins == :acc
-    pc += op - 1 if ins == :jmp
-    pc += 1
-  end
-end
-
-# Run code for part one
-puts execute(code, true)
-
-# Change code for part two
-code.each.with_index do |_, loc|
-  next unless %i{jmp nop}.include?(code[loc][0])
-
-  code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
-  res = execute(code)
-  code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
-
-  puts "#{loc} - #{res}" if res
-end
+# # Parser and lexer ;-)
+# code = File.readlines('input.txt')
+#          .map(&:split)
+#          .map { |is| [is[0].to_sym, is[1].to_i] }
+#
+# # Interpreter
+# def execute(code, no_loops = false)
+#   pc = acc = cycles = 0
+#   coverage = []
+#
+#   while (cycles += 1) < 1000
+#     return acc if pc >= code.length || (coverage[pc] && no_loops)
+#     coverage[pc] = true
+#
+#     ins, op = code[pc]
+#     acc += op    if ins == :acc
+#     pc += op - 1 if ins == :jmp
+#     pc += 1
+#   end
+# end
+#
+# # Run code for part one
+# puts execute(code, true)
+#
+# # Change code for part two
+# code.each.with_index do |_, loc|
+#   next unless %i{jmp nop}.include?(code[loc][0])
+#
+#   code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
+#   res = execute(code)
+#   code[loc][0] = code[loc][0] == :nop ? :jmp : :nop
+#
+#   puts "#{loc} - #{res}" if res
+# end
