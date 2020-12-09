51e31b9ea6604c056bf9c95b9d36144101eec457
Phillip Strom <phillip.strom@gmail.com>
2020-12-08 19:33:02 -0700

    Clean up

--- advent_of_code/day_8/day_8.rb
+++ advent_of_code/day_8/day_8.rb
@@ -1,14 +1,3 @@
-test = ['nop +0',
-'acc +1',
-'jmp +4',
-'acc +3',
-'jmp -3',
-'acc -99',
-'acc +1',
-'jmp -4',
-'acc +6']
-
-
 class GameBuddy
   attr_accessor :instructions, :accumulator, :current_instruction
 
