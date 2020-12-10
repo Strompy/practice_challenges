41180ac6eb2237a9e4bb51334682edaccf58c313
Phillip Strom <phillip.strom@gmail.com>
2020-12-10 12:06:31 -0700

    Solve part 2, had lots of help with the dynamic programming

--- advent_of_code/day_10/day_10.rb
+++ advent_of_code/day_10/day_10.rb
@@ -1,4 +1,4 @@
-data = File.read("input.txt").split("\n").map(&:to_i).sort
+data = File.read("input.txt").split.map(&:to_i).sort
 data.unshift(0)
 data << (data.last + 3)
 
@@ -11,3 +11,12 @@ def part1(data)
 end
 print "Part 1: "
 p part1(data)
+
+
+def dp(adapters, hash, n)
+  return 1 if n == 0
+  return 0 if !adapters.include?(n)
+  hash[n] ||= (1..3).sum { |x| dp(adapters, hash, n - x) }
+end
+print "Part 2: "
+p dp(data, {}, data.last)
