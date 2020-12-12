264f3fd884a5a09dbaf6f2a296ceb19666239f01
Phillip Strom <phillip.strom@gmail.com>
2020-12-12 09:49:11 -0700

    Create method to check adjacent seats

--- advent_of_code/day_11/day_11.rb
+++ advent_of_code/day_11/day_11.rb
@@ -1,1 +1,17 @@
-test = File.readlines('test.txt').map { |line| line.chomp.split('') }
+seats = File.readlines('test.txt').map { |line| line.chomp.split('') }
+# for checking adjacent seats
+X_NAV = [ -1, 0, 1, 1, 1, 0, -1, -1 ]
+Y_NAV = [ -1, -1, -1, 0, 1, 1, 1, 0 ]
+
+height = [0...seats.size]
+width = [0...seats[0].size]
+
+def check_occupied_seats(seats, row, column, height, width)
+  8.times.count do |i|
+    adj_row = row + X_NAV[i]
+    adj_column = column + Y_NAV[i]
+    width.include?(adj_row) && height.include?(adj_column) && seats[adj_row][adj_column] == '#'
+  end
+end
+
+p check_occupied_seats(seats, 0, 0, height, width)
