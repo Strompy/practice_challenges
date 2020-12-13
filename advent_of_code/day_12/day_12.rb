6d44942a5be592802dd1a73d85aed095256bd089
Phillip Strom <phillip.strom@gmail.com>
2020-12-13 09:56:25 -0700

    Part 2 in progress

--- advent_of_code/day_12/day_12.rb
+++ advent_of_code/day_12/day_12.rb
@@ -10,6 +10,7 @@ class Boat
     @x = 0
     @y = 0
     @instructions = instructions
+    @waypoint = Struct.new(:north, :east).new(1, 10)
   end
 
   def navigate
@@ -54,9 +55,37 @@ class Boat
   def manhattan_value
     x.abs + y.abs
   end
+
+  def move_waypoint(direction, steps = 1)
+    case direction
+    when 'E' then @waypoint.east += steps
+    when 'W' then @waypoint.east -= steps
+    when 'N' then @waypoint.north += steps
+    when 'S' then @waypoint.north -= steps
+    end
+  end
+
+  # def move(direction, steps = 1)
+  #   return move_waypoint(direction, steps) if direction != 'F'
+  #
+  #   raise 'Bad Direction' unless direction == 'F'
+  # 
+  #   @north += @waypoint.north * steps
+  #   @east += @waypoint.east * steps
+  # end
+  #
+  # def turn(by: 90)
+  #   waypoint = Complex(@waypoint.north, @waypoint.east) * Complex('i') ** (by / 90)
+  #   @waypoint.north, @waypoint.east = waypoint.real, waypoint.imaginary
+  # end
 end
 
 boat = Boat.new(instructions)
 boat.navigate
 puts "Part 1: "
+p boat.manhattan_value
+
+boat_2 = Boat.new(instructions)
+boat.navigate
+puts "Part 2: "
 p boat.manhattan_value
