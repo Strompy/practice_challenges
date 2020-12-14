d979d9e1ffad6e6767b0b0107b31cfb6ff842013
Phillip Strom <phillip.strom@gmail.com>
2020-12-14 10:30:56 -0700

    Test case working

---
+++ advent_of_code/day_14/day_14.rb
@@ -0,0 +1,21 @@
+inputs = File.readlines('input.txt').map(&:chomp)
+
+mem = Hash.new(0)
+mask = ''
+inputs.each do |line|
+  if line.start_with?('mask')
+    _, mask = line.split(' = ')
+  else
+    memory, value = line.split(' = ')
+    memory = memory[/\d+/]
+    value = value.to_i.to_s(2).rjust(36, '0')
+    mem[memory] = value
+    mask.chars.each_with_index do |char, i|
+      mem[memory][i] = '1' if char == '1'
+      mem[memory][i] = '0' if char == '0'
+    end
+  end
+end
+
+puts "Part 1: "
+print mem.values.map { |num| num.to_i(2) }.sum
