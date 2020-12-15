ec0c719862231de9edbfc9bc1c40619a47a35487
Phillip Strom <phillip.strom@gmail.com>
2020-12-15 11:23:02 -0700

    Solve part 2

--- advent_of_code/day_15/day_15.rb
+++ advent_of_code/day_15/day_15.rb
@@ -1,13 +1,29 @@
-
-van_eck = Enumerator.new do |y|
-  array = [0, 13, 1, 16, 6, 17]
-  loop do
-    y << (term = array.last)  # yield
-    array << (array.count(term)==1 ? 0 : array.size - 1 - array[0..-2].rindex(term))
-  end
+numbers = Hash.new { |h, k| h[k] =[] }
+starting = [0, 13, 1, 16, 6, 17]
+starting.each_with_index do |num, index|
+  numbers[num] << index + 1
 end
+previous_num = numbers.keys.last
+max = 30_000_000
+turns = max - starting.size
 
-game = van_eck.take(2015)
-puts "Part 1: #{game.last}"
-game2 = van_eck.take(29999995)
-puts "Part 2: #{game2.last}"
+turns.times do |turn|
+  first_time = numbers[previous_num].size == 1
+  previous_num = first_time ? 0 : (numbers[previous_num][-1] - numbers[previous_num][-2])
+  numbers[previous_num] << turn + 7
+end
+puts "Part 2: #{previous_num}"
+
+# puts numbers
+# van_eck = Enumerator.new do |y|
+#   array = [0, 13, 1, 16, 6, 17]
+#   loop do
+#     y << (term = array.last)  # yield
+#     array << (array.count(term)==1 ? 0 : array.size - 1 - array[0..-2].rindex(term))
+#   end
+# end
+
+# game = van_eck.take(2015)
+# puts "Part 1: #{game.last}"
+# game2 = van_eck.take(29999995)
+# puts "Part 2: #{game2.last}"
