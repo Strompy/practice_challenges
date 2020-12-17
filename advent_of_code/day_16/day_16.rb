633a98c3e6869f6234e6a1049f5372466f34a3f8
Phillip Strom <phillip.strom@gmail.com>
2020-12-17 09:53:03 -0700

    Solve part 2

--- advent_of_code/day_16/day_16.rb
+++ advent_of_code/day_16/day_16.rb
@@ -33,3 +33,44 @@ invalid = tickets.flat_map do |ticket|
   end
 end
 puts "Part 1: #{invalid.sum}"
+
+valids = tickets.reject do |ticket|
+  ticket.any? do |value|
+    fields.values.none? do |range1, range2|
+      value.between?(*range1) || value.between?(*range2)
+    end
+  end
+end
+
+# require "pry"; binding.pry
+
+
+fields_to_positions = {}
+fields.each do |name, ranges|
+  positions = (0...(valids[0].size)).to_a.select do |i|
+    valids.all? do |ticket|
+      ticket[i].between?(*ranges[0]) || ticket[i].between?(*ranges[1])
+    end
+  end
+  fields_to_positions[name] = positions
+end
+# require "pry"; binding.pry
+
+until fields_to_positions.values.all?(&:one?) do
+  pos_to_remove = fields_to_positions.select { |_, ps| ps.one? }.map(&:last).flatten
+  names_to_remove_from = fields_to_positions.reject { |_, ps| ps.one? }.map(&:first).flatten
+  pos_to_remove.each do |pos|
+    names_to_remove_from.each do |name|
+      fields_to_positions[name].delete(pos)
+    end
+  end
+end
+
+part2 = fields_to_positions
+  .transform_values(&:first)
+  .select { |k, v| k =~ /depart/ }
+  .map(&:last)
+  .map { |i| your_ticket[i] }
+  .reduce(&:*)
+
+puts "part 2: #{part2}"
