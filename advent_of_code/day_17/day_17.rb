31ecd0a0fade0717a04ca64f02f67b7b4988ac38
Phillip Strom <phillip.strom@gmail.com>
2020-12-17 16:06:35 -0700

    This one is tough

--- advent_of_code/day_17/day_17.rb
+++ advent_of_code/day_17/day_17.rb
@@ -14,33 +14,88 @@ def count_neighbors(neighbors, coordinate, coordinates, to_make)
   neighbors.count do |n|
     cube = [n, coordinate].transpose.map {|x| x.reduce(:+)}
     to_make << cube if coordinates[cube].nil?
-    # coordinates[cube] ||= false
     coordinates[cube] == true
   end
 end
 
+
 6.times do |i|
+  # new_state = {}
   to_change = []
   to_make = []
   coordinates.each do |coord, bool|
     count = count_neighbors(neighbors, coord, coordinates, to_make)
+    # require "pry"; binding.pry
     if bool == true
-      next if count == 2 || count == 3
-      to_change << coord
+      if count == 2 || count == 3
+        # new_state[coord] = true
+      else
+        to_change << coord
+        # new_state[coord] = false
+      end
     elsif bool == false && count == 3
       to_change << coord
+      # new_state[coord] = true
+    # else
+      # new_state[coord] = bool
     end
   end
-
+# require "pry"; binding.pry
+  # new_state = coordinates
+# require "pry"; binding.pry
   to_change.each do |coord|
     coordinates[coord] = !coordinates[coord]
   end
-
+# require "pry"; binding.pry
   to_make.each do |coord|
     coordinates[coord] = false
   end
 # require "pry"; binding.pry
 end
 
-puts coordinates.values.count { |c| c == true }
-require "pry"; binding.pry
+puts coordinates.values.count { |c| c == true } #answer 353
+# require "pry"; binding.pry
+
+space = Hash.new('.')
+File.read('input.txt').lines.map(&:chomp).each.with_index do |row, x|
+  row.each_char.with_index { |c, y| space[[ x, y, 0, 0 ]] = c }
+end
+
+6.times do
+  copy = space.dup
+
+  # bounds
+  coords = space.keys
+  mx, nx = coords.map { |c| c[0] }.min - 1, coords.map { |c| c[0] }.max + 1
+  my, ny = coords.map { |c| c[1] }.min - 1, coords.map { |c| c[1] }.max + 1
+  mz, nz = coords.map { |c| c[2] }.min - 1, coords.map { |c| c[2] }.max + 1
+  mw, nw = coords.map { |c| c[3] }.min - 1, coords.map { |c| c[3] }.max + 1
+
+  # advance
+  mx.upto(nx).each do |x|
+    my.upto(ny).each do |y|
+      mz.upto(nz).each do |z|
+        mw.upto(nw).each do |w|
+          adj = 0
+          -1.upto(1).each do |dx|
+            -1.upto(1).each do |dy|
+              -1.upto(1).each do |dz|
+                -1.upto(1).each do |dw|
+                  next if dx == 0 && dy == 0 && dz == 0 && dw == 0
+                  adj += 1 if space[[ x + dx, y + dy, z + dz, w + dw ]] == '#'
+                end
+              end
+            end
+          end
+
+          copy[[ x, y, z, w ]] = case space[[ x, y, z, w ]]
+          when '#' then [ 2, 3 ].include?(adj) ? '#' : '.'
+          when '.' then adj == 3 ? '#' : '.'
+          end
+        end
+      end
+    end
+  end
+  space = copy
+end
+p space.values.count { |c| c == '#' }
