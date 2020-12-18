423735912691aede0916da1f71607d197631b54a
Phillip Strom <phillip.strom@gmail.com>
2020-12-18 11:21:18 -0700

    First test case passes

---
+++ advent_of_code/day_18/day_18.rb
@@ -0,0 +1,45 @@
+equations = []
+File.readlines('input.txt').each do |line|
+  equations << line.chomp
+end
+
+test = '1 + 2 * 3 + 4 * 5 + 6'
+
+def solve(input)
+  i = 0
+  sum = nil
+  num = nil
+  operator = nil
+  until i >= input.length
+    case input[i] #parse char
+    when '+'
+      operator = '+'
+      i += 1
+      next
+    when '*'
+      operator = '*'
+      i += 1
+      next
+    when ' '
+      i += 1
+      next
+    when '('
+      require "pry"; binding.pry
+      # change index to skip to after the closing parenthesis
+    else
+      num = input[i].to_i
+      i += 1
+    end
+    # do the math
+    if sum.nil?
+      sum = num
+    elsif operator == '+'
+      sum += num
+    elsif operator == '*'
+      sum *= num
+    end
+  end
+  sum
+end
+
+puts solve(test)
